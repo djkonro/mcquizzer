27a9a1d4a4ba105e8226273d7cdcb8fb4f36b3f8
djkonro <djkonro35@gmail.com>
2015-10-19 10:03:12 +0100

    Added question class(model)

--- quizer/models.py
+++ quizer/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200) 
+    def __str__(self):              # __unicode__ on Python 2
+        return self.question_text
